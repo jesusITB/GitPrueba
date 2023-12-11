859653e8ced45b971ea6ebac04f11c663e12bdae
jesusITB <jesus.gomez.7e3@itb.cat>
2023-12-11 13:20:49 +0100

    Added input validation. Closes #2

--- githubPrueba/githubPrueba/Program.cs
+++ githubPrueba/githubPrueba/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace yisus
+{
+    public class Yisus
+    {
+        public static void Main(string[] args)
+        {
+            const string msg = "Que area quieres calcular" +
+                "triangulo" +
+                "cuadrado" +
+                "circulo";
+
+            Console.WriteLine(msg);
+            string respu = Console.ReadLine();
+            Console.WriteLine(fun(respu.ToLower()));
+
+
+
+        }
+        public static int fun(string respu)
+        {
+            const string Base = "introduce la base";
+            const string altura = "introduce la altura";
+            const string lado = "introduce uno de los lado";
+            const string radio = "introduce el radio";
+
+            int BaseNum, alturaNum;
+            double radioNum;
+            int LadoOne, LadoTwo;
+            double pi = 3.14159265359;
+
+            int resultado = 0;
+            switch (respu)
+            {
+                case "triangulo":
+
+                    Console.WriteLine(Base);
+                    BaseNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(altura);
+                    alturaNum = Convert.ToInt32(Console.ReadLine());
+
+                    resultado = (BaseNum * alturaNum) / 2;
+                    break;
+                case "cuadrado":
+                    Console.WriteLine(lado);
+                    LadoOne = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(lado);
+                    LadoTwo = Convert.ToInt32(Console.ReadLine());
+
+                    resultado = (LadoOne * LadoTwo);
+
+                    break;
+                case "circulo":
+
+                    Console.WriteLine(radio);
+                    radioNum = Convert.ToDouble(Console.ReadLine());
+
+                    resultado = (int)(pi * Math.Sqrt(radioNum));
+
+                    break;
+
+            }
+            return resultado;
+        }
+    }
+}
